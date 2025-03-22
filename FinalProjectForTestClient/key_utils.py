9bb9c8a72a02df090fff2fb16a4bb173e586c224
Amaanullah Shahim <amaanullahshahim@protonmail.com>
2025-03-21 19:05:12 -0500

    Update and rename key_utils.py to FinalProjectForTestClient/key_utils.py

--- FinalProjectForTestClient/key_utils.py
+++ FinalProjectForTestClient/key_utils.py
@@ -4,8 +4,13 @@ import time
 from database import insert_key
 
 def generate_and_store_key(expiry=None):
+    """
+    Generates a new RSA key pair and stores the private key within the DB
+    """
+    #Here is where the 2048 bit RSA key pair is generated
     private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     if expiry is None:
         expiry = int(time.time()) + 3600
+    #Key is now stored in the SQLite DB and retrieve the assigned ID/kid
     kid = insert_key(private_key, expiry)
     return kid, private_key, expiry
