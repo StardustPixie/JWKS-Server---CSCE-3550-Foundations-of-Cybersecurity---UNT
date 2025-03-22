02548a0834e02b5cf4ded2b18f10df75fc53e5e7
Amaanullah Shahim <amaanullahshahim@protonmail.com>
2025-03-21 19:06:36 -0500

    Update and rename myapp.py to FinalProjectForTestClient/myapp.py
    Added descriptive comments

--- FinalProjectForTestClient/myapp.py
+++ FinalProjectForTestClient/myapp.py
@@ -7,12 +7,14 @@ from routes import jwks_blueprint, auth_blueprint
 def create_app():
     app = Flask(__name__)
 
+    #DB is initilizaed and ensures that the keys table exists
     database.init_db()
 
     if not database.get_all_valid_keys():
         key_utils.generate_and_store_key(expiry=int(time.time()) + 3600)
         key_utils.generate_and_store_key(expiry=int(time.time()) - 3600)
 
+    #Here is where the route handlers are registered
     app.register_blueprint(jwks_blueprint)
     app.register_blueprint(auth_blueprint)
 
