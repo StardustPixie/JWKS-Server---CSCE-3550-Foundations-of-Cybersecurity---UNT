336a82512c817a41e95b665d4c34cc393e041d1e
Amaanullah Shahim <amaanullahshahim@protonmail.com>
2025-03-21 19:03:05 -0500

    Update and rename jwt_utils.py to FinalProjectForTestClient/jwt_utils.py

--- FinalProjectForTestClient/jwt_utils.py
+++ FinalProjectForTestClient/jwt_utils.py
@@ -1,5 +1,8 @@
 import jwt
 
 def create_jwt(payload, private_key, kid):
+    """
+    Creates a JSON Web Token signed with the provided RSA private key
+    """
     token = jwt.encode(payload, private_key, algorithm='RS256', headers={'kid': str(kid)})
     return token
